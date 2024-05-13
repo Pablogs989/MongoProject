08e5d77608c31a85d5eb440289d64013c42592e7
Pablogs989 <pablogonsan2001@gmail.com>
2024-05-13 10:17:39 +0200

    database connection

---
+++ config/config.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const { MONGO_URI } = require("./keys");
+
+
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error when starting the database");
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
