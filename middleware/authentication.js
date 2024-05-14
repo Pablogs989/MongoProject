67e7a47077abbed8ec04a63bc6456dbc913a968f
Pablogs989 <pablogonsan2001@gmail.com>
2024-05-14 12:54:31 +0200

    delete posts

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const Comment = require('../models/Comment');
+const Post = require('../models/Post');
 const jwt = require('jsonwebtoken');
 const { jwt_secret } = require('../config/keys.js')
 
@@ -27,11 +28,26 @@ const isAdmin = async(req, res, next) => {
     }
     next();
 }
-const isAuthor = async(req, res, next) => {
+const isAuthorPost = async(req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        console.log('====================================');
+        console.log(req.user._id.toString());
+        console.log('====================================');
+        if (post.userId.toString() !== req.user._id.toString()) { 
+            return res.status(403).send({ message: "You don't have permits" });
+        }
+        next();
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ error, message: 'Ha habido un problema al comprobar la autoría' })
+    }
+}
+const isAuthorComment = async(req, res, next) => {
     try {
         const comment = await Comment.findById(req.params._id);
         if (comment.userId.toString() !== req.user._id.toString()) { 
-            return res.status(403).send({ message: 'No tienes permisos' });
+            return res.status(403).send({ message: "You don't have permits"  });
         }
         next();
     } catch (error) {
@@ -40,4 +56,4 @@ const isAuthor = async(req, res, next) => {
     }
 }
 
-module.exports = { authentication, isAdmin, isAuthor };
+module.exports = { authentication, isAdmin, isAuthorComment, isAuthorPost };
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const PostController = require('../controllers/PostController')
 const router = express.Router()
-const { authentication } = require('../middleware/authentication.js')
+const { authentication, isAuthorPost } = require('../middleware/authentication.js')
 
 router.post('/',authentication, PostController.create)
 router.get('/', PostController.getAllPostWithUsersAndComments)
 router.put('/addLike/:id', PostController.postLike)
 router.put('/deleteLike/:id', PostController.deleteLike)
 router.get('/id/:id',PostController.getById)
+router.get('/text/:text',PostController.getByText)
+router.delete('/:id',authentication, isAuthorPost, PostController.delete)
 
 module.exports = router
 
